c0b969955fbe2e070639fdf01cf4399572c88364
zhan-x-l <1653873269@qq.com>
2019-08-07 18:50:03 +0800

    詹孝林提交

---
+++ text2/src/text2/Text1.java
@@ -0,0 +1,15 @@
+package text2;
+
+/**
+ *  ≤‚ ‘‘∂≥Ã≤÷ø‚µƒµÿ÷∑
+ * @author zhanxl
+ *
+ */
+public class Text1 {
+
+	public static void main(String[] args) {
+		System.out.println("git");
+
+	}
+
+}
